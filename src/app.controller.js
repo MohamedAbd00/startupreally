9db66965c27879054e79e22b9b07ea387e2f9d44
bossmostafa0-a11y <bossmostafa0@gmail.com>
2026-07-10 03:26:49 +0300

    Update app.controller.js

--- src/app.controller.js
+++ src/app.controller.js
@@ -14,7 +14,7 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
 app.use(cors({
-  origin: "https://progzila-hfd6.vercel.app",
+  origin: "https://progzilas.vercel.app",
   credentials: true
 }));
 connectDB();

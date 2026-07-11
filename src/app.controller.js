6f520afd6186f3270a3dbb58ac20e0cfef521ae9
bossmostafa0-a11y <bossmostafa0@gmail.com>
2026-07-11 20:06:41 +0300

    Update app.controller.js

--- src/app.controller.js
+++ src/app.controller.js
@@ -14,12 +14,9 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
 app.use(cors({
+  origin: "https://www.progzila.com",
 
-    origin: [
-    "https://progzila.com",
-    "https://www.progzila.com",
-    "http://localhost:5173"
-  ],
+   
   credentials: true
 }));
 connectDB();

e5ebc4b9fb8418ff05bf5d9107a280018a367d70
bossmostafa0-a11y <bossmostafa0@gmail.com>
2026-07-12 13:28:52 +0300

    Update clouid.multern.js

--- src/utlis/multer/clouid.multern.js
+++ src/utlis/multer/clouid.multern.js
@@ -173,22 +173,38 @@ export const uploadToCloudinary = (
     };
 
     // لو الملف جاي من diskStorage
-    if (file.path) {
-      cloudinary.uploader.upload(
-        file.path,
-        options,
-        (error, result) => {
-          if (error) return reject(error);
+   if (file.path) {
+  const uploadMethod =
+    resource_type === "video" || resource_type === "raw"
+      ? cloudinary.uploader.upload_large
+      : cloudinary.uploader.upload;
+console.log({
+  path: file.path,
+  size: fs.statSync(file.path).size,
+  mimetype: file.mimetype,
+  resource_type,
+  method:
+    resource_type === "video" || resource_type === "raw"
+      ? "upload_large"
+      : "upload",
+});
+  uploadMethod.call(
+    cloudinary.uploader,
+    file.path,
+    {
+      ...options,
+      chunk_size: 20 * 1024 * 1024,
+    },
+    (error, result) => {
+      if (error) return reject(error);
 
-          fs.unlink(file.path, () => {});
-
-          resolve(result);
-        }
-      );
-
-      return;
+      fs.unlink(file.path, () => {});
+      resolve(result);
     }
+  );
 
+  return;
+}
     // لو الملف جاي من memoryStorage
     const stream = cloudinary.uploader.upload_stream(
       options,

cee3be5758b12e2466a48775c2afe269b8534db8
bossmostafa0-a11y <bossmostafa0@gmail.com>
2026-07-12 13:30:44 +0300

    Update app.controller.js

--- src/app.controller.js
+++ src/app.controller.js
@@ -10,8 +10,8 @@ import notcontroller from "./notification/notification.controller.js";
 
 const bootstrap = (app) => {
 
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.json({ limit: "500mb" }));
+app.use(express.urlencoded({ extended: true, limit: "500mb" }));
 
 app.use(cors({
   origin: "https://progzila.com",
